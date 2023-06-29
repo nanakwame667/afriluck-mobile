58e4419b434ef45bcf6c273ce5552c10b5cd5b69
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2023-06-29 16:22:44 +0000

    Profile updates

--- navigations/MainNavigator.jsx
+++ navigations/MainNavigator.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { View, TouchableOpacity, Image } from "react-native";
-import { Badge } from "react-native-elements";
-import BottomTabNavigator from "./BottomTabNavigator";
-import { useNavigation } from "@react-navigation/native";
 import DrawerNavigator from "./DrawerNavigator";
 import NotificationsScreen from "../screens/NotificationsScreen";
 import { Fonts } from "../theme";
 import colors from "../colors";
+import EditProfile from "../components/Profile/EditProfile";
+import Terms from "../components/Profile/Terms";
+import ChangePassword from "../components/Profile/ChangePassword";
+import PaymentMethod from "../components/Profile/PaymentMethod";
 const Stack = createStackNavigator();
 
 const MainNavigator = () => {
@@ -21,6 +21,50 @@ const MainNavigator = () => {
       <Stack.Screen
         name="Notifications"
         component={NotificationsScreen}
+        options={{
+          headerStyle: { height: 120 },
+          headerBackTitleVisible: false,
+          headerLeftContainerStyle: { paddingLeft: 20 },
+          headerTitleStyle: { fontFamily: Fonts.medium, fontSize: 18 },
+          headerTintColor: colors.heading,
+        }}
+      />
+      <Stack.Screen
+        name="Edit Profile"
+        component={EditProfile}
+        options={{
+          headerStyle: { height: 120 },
+          headerBackTitleVisible: false,
+          headerLeftContainerStyle: { paddingLeft: 20 },
+          headerTitleStyle: { fontFamily: Fonts.medium, fontSize: 18 },
+          headerTintColor: colors.heading,
+        }}
+      />
+      <Stack.Screen
+        name="Update Password"
+        component={ChangePassword}
+        options={{
+          headerStyle: { height: 120 },
+          headerBackTitleVisible: false,
+          headerLeftContainerStyle: { paddingLeft: 20 },
+          headerTitleStyle: { fontFamily: Fonts.medium, fontSize: 18 },
+          headerTintColor: colors.heading,
+        }}
+      />
+      <Stack.Screen
+        name="Terms & Conditions"
+        component={Terms}
+        options={{
+          headerStyle: { height: 120 },
+          headerBackTitleVisible: false,
+          headerLeftContainerStyle: { paddingLeft: 20 },
+          headerTitleStyle: { fontFamily: Fonts.medium, fontSize: 18 },
+          headerTintColor: colors.heading,
+        }}
+      />
+      <Stack.Screen
+        name="Payment Method"
+        component={PaymentMethod}
         options={{
           headerStyle: { height: 120 },
           headerBackTitleVisible: false,
--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,12 +1,61 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
+import CustomText from "../components/CustomText";
+import colors from "../colors";
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      <View>
+        <CustomText
+          weight="medium"
+          style={{ fontSize: 24, color: colors.title }}
+        >
+          My Profile
+        </CustomText>
+      </View>
+      <View style={styles.profileContainer}>
+        <Image
+          style={styles.profile}
+          source={require("../assets/images/avatar.png")}
+        />
+
+        <CustomText
+          weight="medium"
+          style={{ fontSize: 20, color: colors.title }}
+        >
+          Kwame Frimpong
+        </CustomText>
+        <CustomText
+          weight="regular"
+          style={{ fontSize: 18, color: colors.space, paddingVertical: 5 }}
+        >
+          +233 5545 88 483
+        </CustomText>
+      </View>
     </View>
   );
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    paddingHorizontal: 30,
+  },
+  profileContainer: {
+    flexDirection: "column",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 50,
+  },
+  profile: {
+    width: 150,
+    height: 150,
+    borderRadius: 500,
+    marginBottom: 20,
+  },
+});
