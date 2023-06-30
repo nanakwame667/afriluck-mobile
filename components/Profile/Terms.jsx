19a9b502460d3b5c4bcaa9c78d40556aabf2690c
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2023-06-30 19:45:05 +0000

    added terms and conditions

--- components/Profile/Terms.jsx
+++ components/Profile/Terms.jsx
@@ -1,12 +1,104 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { TermsData } from "../../data/Terms";
+import { FlatList } from "react-native-gesture-handler";
+import CustomText from "../CustomText";
+import colors from "../../colors";
+import { Fonts } from "../../theme";
+
+const Item = ({ title, description }) => {
+  return (
+    <View style={styles.item}>
+      <View>
+        <CustomText>{title}</CustomText>
+      </View>
+      <View>
+        <CustomText>{description}</CustomText>
+      </View>
+    </View>
+  );
+};
 
 const Terms = () => {
-  return (
+  const renderHeader = () => (
     <View>
-      <Text>Terms</Text>
+      <CustomText
+        weight="regular"
+        style={{
+          fontSize: 14,
+          color: colors.primary,
+          marginTop: 20,
+          textAlign: "left",
+          marginHorizontal: 30,
+        }}
+      >
+        These Terms and Conditions of Use (“Terms”) govern your access and use
+        of the betting platform (“Platform”) offered by Bettipster (“Company”).
+        By accessing or using the Platform, you agree to be bound by these
+        Terms. If you do not agree to these Terms, do not access or use the
+        Platform.
+      </CustomText>
+    </View>
+  );
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={TermsData}
+        ListHeaderComponent={renderHeader}
+        renderItem={({ item }) => (
+          <Item title={item.title} description={item.description} />
+        )}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
 
 export default Terms;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: colors.background,
+  },
+  profileContainer: {
+    flexDirection: "column",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 30,
+  },
+  profile: {
+    width: 120,
+    height: 120,
+    borderRadius: 500,
+    marginBottom: 20,
+  },
+  indicatorContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 60,
+    height: 60,
+    backgroundColor: "#E2F7F8",
+    borderRadius: 50,
+  },
+  list: {
+    flex: 1,
+  },
+  item: {
+    width: "100%",
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+  },
+  perItem: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    alignItems: "center",
+  },
+});
---
+++ data/Terms.js
@@ -0,0 +1,26 @@
+export const TermsData = [
+  {
+    id: 1,
+    title: "1- Eligibility",
+    description: `You must be at least 18 years of age and be a resident of a jurisdiction where the use of the Platform is lawful to access or use the Platform. By using the Platform, you represent and warrant that you meet these eligibility requirements.
+`,
+  },
+  {
+    id: 2,
+    title: "2- Account Registration",
+    description: `To use the Platform, you must create an account by providing accurate and complete information about yourself. You agree to keep your account information updated and to maintain the confidentiality of your login credentials. You are responsible for all activity that occurs under your account.
+`,
+  },
+  {
+    id: 3,
+    title: "3- Deposits and Withdrawals ",
+    description: `You may deposit funds into your account using the payment methods available on the Platform. You agree to only use payment methods that you are authorized to use. You may withdraw funds from your account subject to the withdrawal policies and limits of the Platform.
+`,
+  },
+  {
+    id: 4,
+    title: "4- Deposits and Withdrawals ",
+    description: `You may deposit funds into your account using the payment methods available on the Platform. You agree to only use payment methods that you are authorized to use. You may withdraw funds from your account subject to the withdrawal policies and limits of the Platform.
+`,
+  },
+];
